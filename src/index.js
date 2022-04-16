7598f30c04f6e2aee0cc63c786280ef58d363884
JoyTriza <wangarijoytriza@gmail.com>
2022-04-16 03:21:08 +0300

    Completed task

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const init = () => {
     const inputForm = document.querySelector('form');
     inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    // console.log(event);
+    
     const input = document.querySelector('input#searchByID');
 
 
